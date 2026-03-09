0509262d8b038170feaed0fa025d69f44d968b0f
Prabhakar Dubey <prabhakar111ji@gmail.com>
2026-03-10 00:22:24 +0530

    Time: 38 ms (94.29%) | Memory: 99.2 MB (94.53%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a,b)->{
+            if(a[0] == b[0])return b[1] - a[1];
+            return a[0] - b[0];
+        });
+        int[] dp = new int[envelopes.length];
+        int len = 0;
+        for(int[] e : envelopes){
+            int h = e[1];
+            int i = Arrays.binarySearch(dp,0,len,h);
+            if(i<0)i=-(i+1);
+            dp[i] = h;
+            if(i==len)len++;
+        }
+        return len;
+    }
+}
