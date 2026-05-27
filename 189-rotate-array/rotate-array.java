41b69916a4991c31a88ed5f7c1ba13f059c464bb
Prabhakar Dubey <prabhakar111ji@gmail.com>
2026-05-27 11:13:32 +0530

    Time: 0 ms (100.00%) | Memory: 61.3 MB (39.29%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
-        k = k%n;
+        k = k%n;// Most Imp because It will rotate only in size, prevent outside to go pointer
         rev(nums, 0, n-k-1);
         rev(nums, n-k, n-1);
         rev(nums, 0, n-1);
