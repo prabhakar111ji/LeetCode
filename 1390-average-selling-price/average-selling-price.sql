e382e36ce666eb0ac7a9e2d654ee42f34a27893e
Prabhakar Dubey <prabhakar111ji@gmail.com>
2026-06-19 15:25:14 +0530

    Time: 714 ms (99.73%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -2,7 +2,8 @@
 SELECT 
         p.product_id, 
         ROUND(
-            COALESCE(SUM(p.price * u.units)/SUM(u.units),0)
+            -- COALESCE(SUM(p.price * u.units)/SUM(u.units),0)
+            IFNULL(SUM(p.price * u.units)/SUM(u.units),0)
             ,2
     ) AS average_price
 FROM Prices AS p
