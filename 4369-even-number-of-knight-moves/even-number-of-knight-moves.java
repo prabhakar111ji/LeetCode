3248aad2672411f6003635fba5bb9e0aca535a60
Prabhakar Dubey <prabhakar111ji@gmail.com>
2026-07-22 20:14:40 +0530

    Time: 1 ms (76.79%) | Memory: 44.2 MB (39.86%) - LeetSync

--- 4369-even-number-of-knight-moves/even-number-of-knight-moves.java
+++ 4369-even-number-of-knight-moves/even-number-of-knight-moves.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean canReach(int[] start, int[] target) {
-        return (start[0] + start[1])%2 == (target[0] + target[1])%2;
+        // first sol
+        // return (start[0] + start[1])%2 == (target[0] + target[1])%2;
+        
+        // second sol
+        return (Math.abs(start[1] - start[0]) & 1) == (Math.abs(target[1] - target[0]) & 1);
     }
 }
